bb22f1ca157ab79749047ba865ea14d56546f1e5
shadowdestructor <yasinyildirim27@outlook.com>
2022-12-28 16:42:34 +0300

    idle ferris

---
+++ Assets/_GameData/Scripts/_Controllers/PlayerController.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    /*public void Click()
+    {
+        Time.timeScale = 5f;
+        Invoke(nameof(NormalTime),.5f);   
+    }
+    private void NormalTime()=> Time.timeScale = 1f;*/
+}
